452d0cd4c993d88f57df9ff89ba0f2c548af9432
Olivia-tou <olivia_tou@yahoo.co.jp>
2015-07-01 21:04:23 +0900

    update compound

--- spec/compound_spec.rb
+++ spec/compound_spec.rb
@@ -6,15 +6,22 @@ describe "Ootalk::compound class" do
    var1 = OoTalk::Variable.new('n')
    var2 = OoTalk::Variable.new('s')
    con = OoTalk::Constant.new(1)
+   # n > 0
    gt =  OoTalk::GreaterThan.new(var1,OoTalk::Constant.new(0))
+   # n = 5
    ass1 = OoTalk::Assignment.new(var1,OoTalk::Constant.new(5))
+   # s = 1
    ass2 = OoTalk::Assignment.new(var2,con)
+   # s = s*n
    asst = OoTalk::Assignment.new(var2,OoTalk::Times.new(var2,var1))
+   # n-1
    sub = OoTalk::Substract.new(var1,con)
+   # n = n-1
    ss = OoTalk::Assignment.new(var1,sub)
+   # unless(n > 0);loop((s= s*n);(n = n-1));
    lp = OoTalk::Loop.new(gt,asst,ss)
 
-   # n = 5; x = 1; loop((s= s*n);(n = n-1));
+   # n = 5; x = 1; unless(n > 0);loop((s= s*n);(n = n-1));
    com =  OoTalk:: Compound.new(ass1, OoTalk:: Compound.new(ass2,OoTalk:: Compound.new(lp,var2)))
 
     it "should be initialized properly" do
