434740145badaa5d83d1df21a567e40c5610a3a5
Olivia-tou <olivia_tou@yahoo.co.jp>
2015-07-01 19:29:20 +0900

    update

--- lib/ootalk/compound.rb
+++ lib/ootalk/compound.rb
@@ -2,5 +2,20 @@ require 'ootalk/instruction'
 
 module OoTalk
   class Compound < Instruction
+    private
+     def initialize(left, right)
+       @left = left
+       @right = right
+     end
+
+   public
+     def exec
+       @left.exec
+       return @right.exec
+     end
+    
+     def to_s
+       #{@left}";"#{@right}
+     end
   end
 end
