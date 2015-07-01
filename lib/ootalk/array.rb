7ad43f191b2c16c86a2eebaf354070e9322021f0
Olivia-tou <olivia_tou@yahoo.co.jp>
2015-07-02 06:49:30 +0900

    add comment to Array class

--- lib/ootalk/array.rb
+++ lib/ootalk/array.rb
@@ -9,12 +9,15 @@ module OoTalk
      end
 
      public
-
+      #@array.map(&:exec):block{}の中で、式列一個づつ実行する
+      #@array.last.exec:最後の結果を返す
+      #式列実行してから、最後の結果を返す
       def exec
         @array.map(&:exec)
         return @array.last.exec
      end
-
+     
+     #式列の表示
       def to_s
         @array.map(&:to_s)
       end
